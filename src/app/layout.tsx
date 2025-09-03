81299f5e550a16410bcd6027fcf9491386e1dd0d
Armaan Patankar <armaan_patankar@brown.edu>
2025-09-03 14:33:22 -0400

    reset

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,18 +8,6 @@ const robotoMono = Roboto_Mono({
 
 import "../styles/globals.css";
 
-export const metadata = {
-  title: "Deep Learning Course",
-  description: "Welcome to the Deep Learning Course website",
-  viewport: {
-    width: "device-width",
-    initialScale: 1,
-    maximumScale: 1,
-    userScalable: false,
-    viewportFit: "cover",
-  },
-};
-
 export default function RootLayout({
   children,
 }: {
--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -1,4 +1,11 @@
 export const metadata = {
   title: "Deep Learning Course",
   description: "Welcome to the Deep Learning Course website",
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+    maximumScale: 1,
+    userScalable: false,
+    viewportFit: "cover",
+  },
 };
